d960cbf593f18f80b6c22b0d2d1cff5a19971971
Dantas198 <33515744+Dantas198@users.noreply.github.com>
2018-10-05 21:57:19 +0100

    Alunos e getClube

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,13 +1,54 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Aluno {
 
     private String nome;
     private int numero;
+    private String curso;
+    private LocalDate ano;
+    private String morada;
+
 
     public Aluno() {
-
     }
 
-    //fazer clone
+    public Aluno(String nome, int numero, String curso, LocalDate ano, String morada){
+        this.nome = nome;
+        this.numero = numero;
+        this.curso = curso;
+        this.ano = ano;
+        this.morada = morada;
+    }
+    public Aluno(Aluno a){
+        this.nome = a.getNome();
+        this.numero = a.getNumero();
+        this.curso = a.getCurso();
+        this.ano = a.getAno();
+        this.morada = a.getMorada();
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public LocalDate getAno() {
+        return ano;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public int getNumero(){
+        return numero;
+    }
+    public Aluno clone(){
+        return new Aluno(this);
+    }
 }
